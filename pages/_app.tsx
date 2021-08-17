5c264aee6b653620afc366d73049d8ac97d0a992
jiwon <jidole041214@naver.com>
2021-08-17 22:43:49 +0900

    [UPDATE] INIT

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
-import { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>KUNDER</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css"
+        />
+        <link rel="icon" href="/logo.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ styles/index.ts
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const WRAPPER = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const CONTAINER = styled.div`
+  width: 1150px;
+`;
+
+export const COLOR = {
+  black: "#343434",
+  main: "#343434",
+  text: "#848484",
+  red: "#ff2d2d",
+};

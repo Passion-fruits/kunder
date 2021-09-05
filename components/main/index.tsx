417e90d565feb9e87e7f9034b0149afa8ec6a49b
jiwon <jidole041214@naver.com>
2021-09-05 22:36:22 +0900

    [UPDATE]

--- components/main/index.tsx
+++ components/main/index.tsx
@@ -7,9 +7,11 @@ export default function MainPage() {
     <S.Wrapper>
       <S.Container>
         <MainTopContainer />
-        <ThemaCardList title="최신 힙합음악을 만나보세요" genre="힙합" />
-        <ThemaCardList title="분위기 좋은 날, 재즈" genre="재즈" />
+        <ThemaCardList title="나를 힙하게 만드는 음악, 힙합" genre="힙합" />
+        <ThemaCardList title="분위기 좋은 날엔, 재즈" genre="재즈" />
         <ThemaCardList title="당신을 즐겁게 할 팝 음악" genre="팝" />
+        <ThemaCardList title="감성을 복돋는 음악, 클래식" genre="클래식" />
+        <ThemaCardList title="당신만을 위한 레게 음악" genre="레게" />
       </S.Container>
     </S.Wrapper>
   );

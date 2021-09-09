30365a78ef36be8080223af681ed496f57495d72
jiwon <jidole041214@naver.com>
2021-09-09 15:00:15 +0900

    [ADD] 토큰 문제 관련

--- api/axios.ts
+++ api/axios.ts
@@ -35,11 +35,15 @@ instance.interceptors.response.use(
       response: { status },
     } = error;
     const originalRequest = config;
+    // 만약 토큰 오류가 났으면
     if (status === 401) {
+      // 만약 로컬스토리지에 리프레쉬 토큰이 박혀있으면
       if (!isTokenRefreshing && localStorage.getItem(REFRESH_TOKEN)) {
         // isTokenRefreshing이 false인 경우에만 token refresh 요청
         isTokenRefreshing = true;
+        // 리프레쉬 받아와
         const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        // 리프레쉬로 토큰 재 요청 해
         axios({
           url: `${process.env.NEXT_PUBLIC_API_DOMAIN}refresh`,
           method: "get",
@@ -49,21 +53,26 @@ instance.interceptors.response.use(
           },
         })
           .then((res) => {
+            // 요청 잘 됬으면
             const data = res.data;
             const accessToken = data.access_token;
-            // 새로운 토큰 저장
+            // 새로운 토큰으로 엑세스토큰 갈아 끼워
             localStorage.setItem(ACCESS_TOKEN, accessToken);
             isTokenRefreshing = false;
             axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-            // 새로운 토큰으로 지연되었던 요청 진행
+            // 401떳던 요청 다시 진행 시켜
             onTokenRefreshed(accessToken);
           })
-          .catch((err) => {
+          .catch(() => {
+            // 만약 에러가 났어
+            // 엑세스랑 리프레쉬 둘다 비워버려
             localStorage.setItem(ACCESS_TOKEN, "");
             localStorage.setItem(REFRESH_TOKEN, "");
             return Promise.reject(error);
           });
       } else {
+        // 리프레쉬 토큰이 없어
+        // 둘다 비워버려
         localStorage.setItem(ACCESS_TOKEN, "");
         localStorage.setItem(REFRESH_TOKEN, "");
         return Promise.reject(error);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import Footer from "../components/footer";
 import MainPage from "../components/main";
+import React from "react";
+import profile from "../api/profile";
 export default function Home() {
+  React.useEffect(() => {
+    profile.checkFollow(1000);
+  }, []);
   return (
     <>
       <MainPage />
